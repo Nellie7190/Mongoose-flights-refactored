f7efb82d9d1cb3202eb8766387abaa4b895fceaa
Your Name <nelliebosch@Nellies-MacPro.lan>
2023-04-07 17:22:26 -0400

    added tickets model and ticket(show) to views/flights/show page

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,4 +1,6 @@
 const Flight = require('../models/flight');
+const Ticket = require('../models/ticket');
+
 
 module.exports = {
     index,
@@ -17,21 +19,24 @@ async function index(req, res) {
 // flights show
 async function show(req, res) {
     const flight = await Flight.findById(req.params.id)
+    const tickets = await Ticket.find({flight: flight._id})
     //  pass both the flight and tickets in the res.render call
-    res.render("flights/show", { title: "Flight Detail", flight });
+    res.render("flights/show", { title: "Flight Detail", flight, tickets });
   }
 
-// new flight path so we render it
+// new flight path so we render form
 function newFlight(req, res) {
     res.render('flights/new', {title: 'Add A Flight'});
 }
 
+// flight controller for posting new from form
 function createFlight(req, res) { 
     const flight = new Flight(req.body);
     flight.save()
     res.redirect('/flights');
 }
 
+// deleting specific flight
 async function deleteFlight(req, res) {
     const deletedFlight = await Flight.findByIdAndDelete(req.params.id)
     res.redirect('/flights');
